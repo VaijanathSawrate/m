f056db21ebde9672559f07029dbf8087ce4f28f0
Vaijanth Sawrate <75881654+VaijanathSawrate@users.noreply.github.com>
2020-12-28 20:25:54 +0530

    spyder_file

---
+++ 2.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Jul 19 23:40:29 2020
+
+@author: LENOVO
+"""
+
+
+n=input("What is your name?")
+print("Hello" , n,"!")
+print("How are you",n,".")
+print("I hope, life is treating",n,"well!")
